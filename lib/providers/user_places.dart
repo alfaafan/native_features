47f2e1027baf9c9d362ec61b18c0182dabc2ac24
Alfaafan Yusufa Achmad <BSI80206@bsi.co.id>
2024-01-31 11:32:27 +0700

    feat(places): add places feature

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,18 @@
+//user_places
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:native_features/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+//spread
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
